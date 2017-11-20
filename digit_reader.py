2328ab566a67530be4e7eea5a18704898cd49afd
Unknown <a.rndmized@gmail.com>
2017-11-20 17:27:21 +0000

    Image can now be uploaded and rendered on a canvas.

--- digit_reader.py
+++ digit_reader.py
@@ -1,44 +1,40 @@
 # Source: http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
 
 import os
-from flask import Flask, request, redirect, url_for, render_template
-from werkzeug.utils import secure_filename
+import numpy as np
+from PIL import Image
+from flask import Flask, request, redirect, url_for, render_template, flash, json, jsonify
+from model import model
+import re
+import base64
+from scipy.misc import imread, imresize
+import tensorflow as tf
 
-UPLOAD_FOLDER = './images'
+
+UPLOAD_FOLDER = '/images'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# Parse Image   
+def parseImage(imgData):
+    imgstr = re.search(b'base64,(.*)', imgData).group(1)
+    with open('static/images/ImageResult.png','wb') as output:
+        output.write(base64.decodebytes(imgstr))
 
 @app.route('/', methods=['GET', 'POST'])
 def root():
     return app.send_static_file('index.html')
 
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return app.send_static_file('index.html')
-
+    # Parse the image into the folder before read
+    parseImage(request.get_data())
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
