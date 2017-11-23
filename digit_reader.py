65a07b44c3f48b49e61234d989f84e7f4d6f64cf
Unknown <a.rndmized@gmail.com>
2017-11-23 17:57:00 +0000

    Updated image formatting function.
    * Image discriminates between images with black bacjground and white number and images with white background and black numbers.
    * Updated html to display prediction.
    * Minor changes in appearance.

--- digit_reader.py
+++ digit_reader.py
@@ -14,6 +14,7 @@ import tensorflow as tf
 
 app = Flask(__name__)
 
+# Create tensorflow placeholders and load stored values from trained session
 x = tf.placeholder("float", [None, 784])
 sess = tf.Session()
 
@@ -23,9 +24,10 @@ with tf.variable_scope("deep-learning"):
 save = tf.train.Saver(variables)
 save.restore(sess, "model/deep-learning.ckpt")
 
+# Funtion to predict the number based on an image array
 def predict(image):
     prediction = tf.argmax(y, 1)
-    return prediction.eval(feed_dict={x: image,keep_prob: 1.0}, session=sess)
+    return prediction.eval(feed_dict={x: image, keep_prob: 1.0}, session=sess)
 
 # Parse Image - Handed out by a classmate
 def parseImage(imgData):
@@ -33,11 +35,46 @@ def parseImage(imgData):
     with open('static/images/TestImage.png','wb') as output:
         output.write(base64.decodebytes(imgstr))
 
+# Returns image array with the "proper" 
+# format to be read by the prediction function.
+def setupImage(image):
+    # Resize Image and convert it to grayscale
+    image = image.resize([28,28],resample=pil.LANCZOS).convert(mode='L')
+    image_array = np.ndarray.flatten(np.array(image)).reshape(1, 784)
+    # Detect whether is black & white or viceversa
+    color = 0
+    # Take coloer from first and last row of pixels and add them all together
+    for pix in range(0,28):
+        color += image_array[0][pix]
+        color += image_array[0][-pix]
+
+    # Since there is going to be noise in the white background pictures
+    # Average the background being at least 2/3 white-ish
+    # Remove "noise" from image setting values to be either white or black
+    # depending on the color of the background
+    if (color/2) < ((255*42)/2):
+        # print('Black Background')
+        for pix in range(0,784):
+            if image_array[0][pix] < 150:
+                image_array[0][pix] = 0
+            else:
+                image_array[0][pix] = 255
+    else:
+        # print('White Background')
+        for pix in range(0,784):
+            if image_array[0][pix] > 150:
+                image_array[0][pix] = 0
+            else:
+                image_array[0][pix] = 255
+
+    return image_array
+    
+# Return main page
 @app.route('/', methods=['GET', 'POST'])
 def root():
     return app.send_static_file('index.html')
 
-
+# Take image from request and return prediction for that image
 @app.route('/upload', methods=['POST'])
 def upload_file():
     # Read Image from request
@@ -46,12 +83,7 @@ def upload_file():
     # Format image properly
     # Open Image
     image = pil.open('static/images/TestImage.png')
-    # Resize Image
-    image = image.resize([28,28],resample=pil.LANCZOS)
-    image_gray = image.convert(mode='L')
-    test_image = np.ndarray.flatten(np.array(image_gray)).reshape(1, 784)
-    print(test_image)
-    result = predict(test_image)
+    result = predict(setupImage(image))
     return jsonify(result = str(result[0]))
 
 
